b9bb07d688c206d770671e2c8e071c5378a56baa
zeb <zeb.zhao@gamecheetah.net>
2018-01-27 20:40:36 -0500

    better assert implementation

--- jquip.debug.js
+++ jquip.debug.js
@@ -1189,70 +1189,102 @@ return Promise$2;
       }
       return cond;
     },
-    check: function (obj, schema) {
+    check: function (obj, schema, name, throwError) {
+      var message = errorMessage(stringifySchema(schema), obj, name);
+
       if (schema === Object) {
-        return module.assert(module.isObject(obj),
-          'expected Object got ' + Object.prototype.toString.call(obj), obj);
+        return module.assert(module.isObject(obj), message, obj, throwError);
       }
       else if (schema == Array) {
-        return module.assert(module.isArray(obj),
-          'expected Array got ' + Object.prototype.toString.call(obj), obj);
+        return module.assert(module.isArray(obj), message, obj, throwError);
       }
       else if (schema == String) {
-        return module.assert(module.isString(obj),
-          'expected String got ' + Object.prototype.toString.call(obj), obj);
+        return module.assert(module.isString(obj), message, obj, throwError);
       }
       else if (schema == Boolean) {
-        return module.assert(module.isBoolean(obj),
-          'expected Boolean got ' + Object.prototype.toString.call(obj), obj);
+        return module.assert(module.isBoolean(obj), message, obj, throwError);
       }
       else if (schema == Number) {
-        return module.assert(module.isNumber(obj),
-          'expected Number got ' + Object.prototype.toString.call(obj), obj);
+        return module.assert(module.isNumber(obj), message, obj, throwError);
       }
       else if (schema == Function) {
-        return module.assert(module.isFunction(obj),
-          'expected Function got ' + Object.prototype.toString.call(obj), obj);
+        return module.assert(module.isFunction(obj), message, obj, throwError);
       }
       else if (module.isFunction(schema)) {
-        return module.assert(schema.call(obj, obj),
-          'type check function returned false', obj);
+        return module.assert(schema.call(null, obj),
+          'type check function returned false', obj, throwError);
       }
       else if (module.isObject(schema)) {
-        var match = module.check(obj, Object);
+        var match = module.isObject(obj);
         if (!match) return false;
 
         for (var k in schema) {
           // Tricky, put match last to prevent short circuit
           if (schema.hasOwnProperty(k))
-            match = module.check(obj[k], schema[k]) && match;
+            match = module.check(obj[k], schema[k], k) && match;
         }
-        return match;
+        if (throwError) {
+          throw new Error(message);
+        } else {
+          return match;
+        }
       }
     },
     isArray: function (obj) {
-      return Object.prototype.toString.call(obj) === '[object Array]';
+      return typeOf(obj) === 'Array';
     },
     isString: function (obj) {
-      return Object.prototype.toString.call(obj) === '[object String]';
+      return typeOf(obj) === 'String';
     },
     isObject: function (obj) {
-      return Object.prototype.toString.call(obj) === '[object Object]';
+      return typeOf(obj) === 'Object';
     },
     isDefined: function (obj) {
-      return (Object.prototype.toString.call(obj) !== '[object Null]' &&
-      Object.prototype.toString.call(obj) !== '[object Undefined]');
+      return typeOf(obj) !== 'Null' && typeOf(obj) !== 'Undefined';
     },
     isNumber: function (obj) {
-      return Object.prototype.toString.call(obj) === '[object Number]';
+      return typeOf(obj) === 'Number';
     },
     isBoolean: function (obj) {
-      return Object.prototype.toString.call(obj) === '[object Boolean]';
+      return typeOf(obj) === 'Boolean';
     },
     isFunction: function (obj) {
-      return Object.prototype.toString.call(obj) === '[object Function]';
+      return typeOf(obj) === 'Function';
     }
   });
+
+  // Fix Function#name on browsers that do not support it (IE):
+  if (!(function f() {}).name) {
+    Object.defineProperty(Function.prototype, 'name', {
+      get: function() {
+        var name = (this.toString().match(/^function\s*([^\s(]+)/) || [])[1];
+        Object.defineProperty(this, 'name', { value: name });
+        return name;
+      }
+    });
+  }
+
+  function stringifySchema(schema) {
+    if (typeof schema === 'function') {
+      return schema.name;
+    } else {
+      var result = '{';
+      for (var k in schema) {
+        // Tricky, put match last to prevent short circuit
+        if (schema.hasOwnProperty(k))
+          result += k + ':' + stringifySchema(schema[k]) + ', ';
+      }
+      return result.slice(0, -2) + '}';
+    }
+  }
+
+  function typeOf(obj) {
+    return Object.prototype.toString.call(obj).slice(8, -1);
+  }
+
+  function errorMessage(type, obj, name) {
+    return 'expected ' + type + (name ? ' for ' + name : '') + ', but got ' + typeOf(obj) + ' instead';
+  }
 })(jQuip);
 
 window.jQuip = window.$Q = (function (exports) {
